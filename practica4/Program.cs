df8d8ae8df29b0b3b8803b8825bb2ddadf878a4c
Gama´s Product <Gamas Product@DESKTOP-S14JKI4>
2022-09-26 20:31:23 -0700

    Agregar archivos de proyecto.

---
+++ practica4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practica4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int centigrados, farenheit;
+
+            Console.WriteLine("dame los centigrados");
+            centigrados = Convert.ToInt32(Console.ReadLine());
+            farenheit = (centigrados * 9 / 5 + 32);
+            Console.WriteLine("escribiste:" + centigrados + "que es igual a:" + farenheit +"");
+            Console.ReadLine();
+        }
+    }
+}
